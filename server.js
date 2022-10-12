c3bd37fc9cae213c59fb0652e46a2bb24602ba15
Hassan Yossry <hassan.yossry_pg@alexu.edu.eg>
2022-10-12 02:39:05 +0200

    first commit

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { Sequelize, DataTypes, Model } = require("sequelize");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const config = require("./config");
@@ -9,6 +10,35 @@ const app = express();
 
 app.use(express.static("public"));
 app.use(cors());
+
+const sequelize = new Sequelize(
+  'postgres://user:pass@example.com:5432/dbname'
+);
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    avatarURL: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    modelName: "User",
+  }
+);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/index.html"));
@@ -29,10 +59,11 @@ app.use((req, res, next) => {
 });
 
 app.get("/contacts", async (req, res) => {
-  res.send(contacts.defaultData.contacts.map((contact) => ({
-    id: contact.id,
-    name: contact.name,
-    email: contact.email
+  const users = await User.findAll();
+  res.send(users.map(({ dataValues }) => ({
+    id: dataValues.id,
+    name: dataValues.name,
+    email: dataValues.email
   })));
 });
 
@@ -54,9 +85,14 @@ app.post("/contacts", bodyParser.json(), (req, res) => {
 
 app.listen(config.port, async () => {
   try {
+    await sequelize.authenticate();
     console.log("Connection has been established successfully.");
     console.log("Server listening on port %s, Ctrl+C to stop", config.port);
+    await User.sync();
+    contacts.defaultData.contacts.forEach((contact) => {
+      User.findOrCreate({ where: { id: contact.id }, defaults: contact });
+    });
   } catch (error) {
-    console.error("Unable to establish the connection:", error);
+    console.error("Unable to connect to the database:", error);
   }
 });
